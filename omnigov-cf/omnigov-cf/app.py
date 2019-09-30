516842521644a30d1966d42ef7679e8a168b94d4
juan-becerra-work <juanbecerrajm@gmail.com>
2019-09-30 16:49:42 -0300

    Flask project template comments update

--- omnigov-cf/omnigov-cf/app.py
+++ omnigov-cf/omnigov-cf/app.py
@@ -7,9 +7,20 @@ def create_app():
 
     :return: Flask app
     """
+
+    # Says to Flask to find the "instance" module
+    #    in the same folder than the main module
     app = Flask(__name__, instance_relative_config=True)
 
+    # It loads the settings.py file from the "config" module
+    #    where config variables are defined
     app.config.from_object('config.settings')
+
+    # It loads the settings.py file from the "instance" module
+    #    where config variables are defined for PRODUCTION
+    # It overrides the previous line, if setting.py exists
+    # It must be excluded from version control
+    # silent=True says to Flask not to crash if file doesn't exists
     app.config.from_pyfile('settings.py', silent=True)
 
     @app.route('/')
